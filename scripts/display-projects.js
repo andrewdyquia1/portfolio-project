d066cb17b2a428d908b934f5d69df319d4bbabcc
Andrew Dyquiangco <andrewdyquia.93@gmail.com>
2021-11-18 00:06:30 -0800

    switch project order, fix links to email and phone number, add more event listeners to toggle nav, remove hover stylings for certain effects, change styles

--- scripts/display-projects.js
+++ scripts/display-projects.js
@@ -3,7 +3,6 @@ function translateProjects(){
     const projectContainers = document.querySelectorAll('.projects__project-container')
     for(i = 0; i < projectContainers.length; i++){
         const slightlyAboveBottomOfViewportHeight = projectContainers[i].getBoundingClientRect().top - window.innerHeight + 50
-        console.log(projectContainers[0].getBoundingClientRect().top - window.innerHeight)
          if(slightlyAboveBottomOfViewportHeight < 0){
             projectContainers[i].classList.add('projects-translated')
         }
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,13 @@
 const hamburger = document.querySelector('.nav__hamburger')
-
+const navHome = document.querySelector('.nav__link--header')
+const navItems = document.querySelectorAll('.nav__item')
+const navMenu = document.querySelector('.nav__item-container')
+let navActive = false
 function toggleNavMenu(){
-    const navMenu = document.querySelector('.nav__item-container')
     const navBunTop = document.querySelector('.nav__bun--top')
     const navBunMid = document.querySelector('.nav__bun--mid')
     const navBunBot = document.querySelector('.nav__bun--bot')
-    const navItems = document.querySelectorAll('.nav__item')
-
+    
     //toggles whether the navigation menu is active or not through toggling the class
     navMenu.classList.toggle('nav-menu-active')
     navBunTop.classList.toggle('nav__bun--top-active')
@@ -15,6 +16,14 @@ function toggleNavMenu(){
     hamburger.classList.toggle('nav__hamburger-active')
     hamburger.classList.toggle('nav__x-active')
     navItems.forEach(item => item.classList.toggle('nav__item--active'))
+    navActive = !navActive
 }
 
+
 hamburger.addEventListener('click', toggleNavMenu)
+navHome.addEventListener('click', () => {
+    if(navActive){
+        toggleNavMenu()
+    } 
+})
+navItems.forEach(item => item.addEventListener('click', toggleNavMenu))
